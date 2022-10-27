d38bbc023a005572c55e682f1c5650ec34f563e2
madhuAuctionTechs <110889161+madhuAuctionTechs@users.noreply.github.com>
2022-10-27 18:22:13 +0530

    Add files via upload

---
+++ ajax/getprofile.php
@@ -0,0 +1,276 @@
+<style>
+  .left-side {
+  float: left;
+  width: 25%;
+  padding: 1%;
+  margin-left: -31px
+}
+
+.right-side {
+  float: right;
+  width: 25%;
+  padding: 1%;
+}
+
+.content, header {
+  width: 75%;
+  margin: 0 auto;
+}
+
+header {
+  text-align: center;
+}
+
+.content img {
+  max-width: 100%;
+}
+
+
+  </style>
+
+<?php
+
+$id=$_REQUEST['id'];
+
+//echo $id;
+if($id=='1'){
+    
+
+    echo '  
+    <h1 class="modal-title" style="text-align: center;">Madhusudhan Raju</h1>
+    
+    
+    
+        
+              <img class="  left-side" src="assets/img/team/MSRaju_Profile.JPG" srcset="assets/img/team/MSRaju_Profile.JPG 2x" alt="" style="object-fit: cover;" /><br>
+
+              <p>Mr. Madhusudan Raju Mudunuru holds MS degree in software engineering from BITS, Pilani and Advanced Management Programme from IIM Bangalore.</p>
+                
+              <p>He has over 26 years of experience and specialized in various software domains and platforms in the field IT during that period he worked for various companies in India and Abroad. He is a Serial Entrepreneur and Angel Investor.</p>
+              
+              <p>National Award winner for the Outstanding Entrepreneurship for the year 2010 from the Hon\'ble President of India, Smt Pratibha Devisingh Patil.</p>
+              
+              <p>Software Patents : Co-inventor of Patent for the previous employer.</p>
+              
+              <p>Method and System for Flash contents on DRM(DIGITAL RIGHTS) environment, Method and System for Flash contents on DRM(DIGITAL RIGHTS) environment KR 1020020015544
+              .</p>
+              
+              <p>He has over 26 years of experience and specialized in various software domains and platforms in the field IT during that period he worked for various companies in India and Abroad.</p>
+              
+              <nav class="nav social mb-0">
+              <a href="#"><i class="uil uil-twitter"></i></a>
+              <a href="#"><i class="uil uil-linkedin"></i></a>
+              <a href="#"><i class="uil uil-facebook-f"></i></a>
+              <a href="#"><i class="uil uil-dribbble"></i></a>
+          </nav>
+    
+    
+    ';
+    
+    }
+
+    if($id=='2'){
+    
+
+        echo '  
+        
+        <h1 class="modal-title" style="text-align: center;">T Kiran</h1>
+    
+    
+    
+        
+              <img class="  left-side" src="assets/img/team/kiran.png" srcset="assets/img/team/kiran.png 2x" alt="" style="object-fit: cover;" /><br>
+
+              <p>Mr. Kiran has been leading the team to its present strength since its operational. He brings to the board over two decades of experience in diverse technologies in IT Software Development and loads of energy. He has been successful in bringing in mature processes to Mudunuru with his extensive experience in earlier organizations.</p>
+    
+    <p>Prior to joining Mudunuru Group, Kiran has led the IP-PBX R&D team at Mars Telecom (3COM\'s Dedicated R&D Center) and was involved in building the team out here at offshore from the initial stages. He worked with Wipro before moving on to Mars Telecom for over half a decade in diverse technologies and domains from Inventory Management for Aerospace Companies, to Healthcare Management systems and then finally into Telecom Space.</p>
+    
+    <p>In Telecom Space he worked with Wipro\'s Top clients Ericsson and Nokia in areas of ATM Switches and Mobile OSS Applications Space. Kiran holds a MS in Software Engineering from BITS.</p> 
+
+    <nav class="nav social mb-0">
+    <a href="#"><i class="uil uil-twitter"></i></a>
+    <a href="#"><i class="uil uil-linkedin"></i></a>
+    <a href="#"><i class="uil uil-facebook-f"></i></a>
+    <a href="#"><i class="uil uil-dribbble"></i></a>
+</nav>
+        ';
+        
+        } 
+
+if($id=='3'){
+    
+
+echo '  
+
+<h1 class="modal-title" style="text-align: center;">Mudraganam Chandrashekar</h1>
+    
+    
+    
+        
+              <img class="  left-side" src="assets/img/team/Mudraganam_Chandrashekar_2.JPG" srcset="assets/img/team/Mudraganam_Chandrashekar_2.JPG 2x" alt="" style="object-fit: cover;" /><br>
+
+              <p>Chandrashekar has over three decades of expertise in the ICT industry and is passionate about enabling people empowerment and prosperity.</p>
+
+<p>He has held high profile roles in prestigious organisations, such as, Tata Consultancy Services, Tata Trusts, Infronics Systems Ltd., Danlaw Technologies India Ltd., MGRM Ltd., Lokyata, Inc., etc. He has strong work ethics and integrity, and is recognized as a proponent of people empowerment. He is a strategy and business development specialist, with a strong orientation towards business operations, including high level networking and eco-system development.</p>
+
+<p>He is also a Founder – Director of Arziki Consultech Pvt. Ltd., which is a technology driven Processes, Business & Management consulting & advisory firm with expertise in international trade & senior management, having successfully helped MNCs and Global Organizations pursue diverse opportunities in emerging markets.</p>
+
+<p>He has organized and lead several business delegations and trade missions from India to emerging markets and has also facilitated and organized several visits by political, business leaders and senior government officials from emerging markets to India. He regularly organizes seminars, conferences and meetings on business environment in emerging markets for Indian companies, increasing awareness about the opportunities for trade and investment. He has written several articles on economic and technology trends, international markets and current and strategic affairs. He is also a motivational speaker and gives talks on current and strategic issues and spiritual topics.</p>
+
+<p> He is an active member of various social organisations, including Exhibition Society, Osmania Graduates Association and Economic Committee AP, which have an 83-year old history and were established with an aim of furthering economic activity in the Telangana region and provide education in rural areas. These organisations sponsor and manage 19 institutions which provide affordable and quality education from KG to PG to about 30,000 students.</p>
+
+<p>He has an MS degree in Computer Engineering from Wayne State University, Michigan, USA and Bachelor of Engineering in Electronics & Communication from Osmania University, Hyderabad. He is an alumnus of HPS Begumpet.</p>
+
+<p>His resides in Secunderabad with his wife Madhavi who is a Gynecologist, Obstetrician and Laparoscopic Surgeon and son and daughter.</p>
+
+
+<nav class="nav social mb-0">
+<a href="#"><i class="uil uil-twitter"></i></a>
+<a href="https://www.linkedin.com/in/chandrashekar-mudraganam-2b884717" target="_blank"><i class="uil uil-linkedin"></i></a>
+<a href="#"><i class="uil uil-facebook-f"></i></a>
+<a href="#"><i class="uil uil-dribbble"></i></a>
+</nav>
+';
+
+}
+
+
+if($id=='4'){
+    
+
+    echo '  
+    
+    <h1 class="modal-title" style="text-align: center;">Dr. Devsen Kruthiventi</h1>
+    
+    
+    
+        
+              <img class="  left-side" src="assets/img/team/devasen.png" srcset="assets/img/team/devasen.png 2x" alt="" style="object-fit: cover;" /><br>
+
+              <p>Dr K Devsen has 20+ years intense experience in Information Technology and 17 years of teaching and research in mathematics, computer science. He is deeply dedicated to empowering and prospering people. </p>
+
+<p>He formerly held top positions at Satyam, Capgmini, Tata Chemicals, Tata Projects, and Tata Trusts. He has published papers in top national and international publications on IT, content management, architecture, SaaS, Cloud Computing, SOA, ERP, IT security, KM, and ECM, including IEEE Transactions on Knowledge and Data Engineering, Mathematical Education, Transport and Assimilates, Mathematical Modeling in Science and Technology, ICMMST, and Knowledge Management Reviews. </p>
+
+<p> He also contributed Chapter 14 to the second edition of "Knowledge Management Handbook: Collaboration and Social Networking." He has given almost a hundred lectures in diverse fields.</p>     
+<p>In his spare time, he enjoys volunteering in social work activities and is an active member of several social and religious groups, including the Sri Sathya Sai Organization.</p>
+<p>Awards & Recognitions:</p>  
+<div class="row gy-3 gx-xl-8">
+<div class="col-xl-12">
+  <ul class="icon-list bullet-bg bullet-soft-primary mb-0">
+    <li><span><i class="uil uil-check"></i></span><span>“Global Knowledge Management Leader” by World Education Congress in June 2016</span></li>
+    <li><span><i class="uil uil-check"></i></span><span>“50 Most Influential People in Knowledge Management Professionals Award” by World Education Congress in July 2015.</span></li>
+    <li><span><i class="uil uil-check"></i></span><span>“Knowledge Management Leadership Award” by Global Knowledge Management Congress 2014 in August 2014.
+    </span></li>
+    <li><span><i class="uil uil-check"></i></span><span>Key Note Speaker at KM Iran 2016</span></li>
+    <li><span><i class="uil uil-check"></i></span><span>“MAKE India and MAKE Asia” award in August and November 2011 on behalf of Tata Chemicals for implementation of KM system</span></li>
+    <li><span><i class="uil uil-check"></i></span><span></span>Consulted Nepal Administrative college for Knowledge Audit in 2014</li>
+    <li><span><i class="uil uil-check"></i></span><span>Created strategy and roadmap for KM rollout in Tata Power in 2006</span></li>
+    <li><span><i class="uil uil-check"></i></span><span>American Productivity and Quality Center (APQC) Best Practices Partner for 2012, 2013.</span></li>
+    <li><span><i class="uil uil-check"></i></span><span>Guest Faculty at Indian Institute of Management, Ahmadabad, Administrative Staff College of India</span></li>
+    <li><span><i class="uil uil-check"></i></span><span>Creation of IT Enterprise Architecture, IT Roadmap for a major Infrastructure Company</span></li>
+    <li><span><i class="uil uil-check"></i></span><span>Experience in selecting an IT partner for a major Infrastructure company.</span></li>
+    <li><span><i class="uil uil-check"></i></span><span>Recognition for setting up Impact and Knowledge Management Center for fourth Largest Philanthropic organization.
+    </span></li>
+            </ul>
+          </div>
+          </div>
+          <p>He is currently serving as Director At Reinvision Labs and Advisor to Adani Power, Coromandel, Unitol Solutions.
+          </p>
+          <nav class="nav social mb-0">
+          <a href="#"><i class="uil uil-twitter"></i></a>
+          <a href="#"><i class="uil uil-linkedin"></i></a>
+          <a href="#"><i class="uil uil-facebook-f"></i></a>
+          <a href="#"><i class="uil uil-dribbble"></i></a>
+      </nav>
+    ';
+    
+    } 
+
+    if($id=='5'){
+    
+
+        echo '  
+        
+        <h1 class="modal-title" style="text-align: center;">Gopal Ramesh</h1>
+    
+    
+    
+        
+        <img class="  left-side" src="assets/img/team/ramesh.png" srcset="assets/img/team/ramesh.png 2x" alt="" style="object-fit: cover;" /><br>
+
+        <p>Mr. Gopal Ramesh is Technocrat and Entrepreneur and has over 2 Decades of expertise in building robust products and solutions for Various Domains and Customers. Has been a Guest Lecturer, Digital Artist, Expert Photographer, Archaeologist, Business Analyst and got Strong footing in Web Based Graphical Domain based on Mathematical Models. He pioneered in Several Products in EdTech Domain starting from Education Intelligence to Learning Management Systems.</p>
+    
+    <p>He Built Advanced Products in EdTech –Unique Assessment Products, Outcome Based Learning Systems, Strategic Planning and Event Mgmt. Products for Advanced Display and Teaching Tools. He holds Master degree in Information Technology and Management.
+    </p>
+    <nav class="nav social mb-0">
+    <a href="#"><i class="uil uil-twitter"></i></a>
+    <a href="#"><i class="uil uil-linkedin"></i></a>
+    <a href="#"><i class="uil uil-facebook-f"></i></a>
+    <a href="#"><i class="uil uil-dribbble"></i></a>
+</nav>
+        
+        ';
+        
+        }
+
+
+        if($id=='6'){
+    
+
+            echo '  
+            
+            <h1 class="modal-title" style="text-align: center;">Venkata Ramesh Annamreddy</h1>
+    
+    
+    
+        
+              <img class="  left-side" src="assets/img/team/ramesh2.png" srcset="assets/img/team/MSRaju_Pramesh2.png 2x" alt="" style="object-fit: cover;" /><br>
+
+               <p>Mr. Venkata is Leading Serial Entrepreneur based out of USA and he is President at Sneha Media. He is the current CEO at GEMS Inc (Global Enterprise Management Solutions) for Over 2 Decades He holds MBA from University of Virginia Darden School of Business.</p>
+        
+              <p>Mr. Venkata is Leading Serial Entrepreneur based out of USA and he is President at Sneha Media. He is the current CEO at GEMS Inc (Global Enterprise Management Solutions) for Over 2 Decades.
+      
+              </p>
+          
+              <nav class="nav social mb-0">
+              <a href="#"><i class="uil uil-twitter"></i></a>
+              <a href="#"><i class="uil uil-linkedin"></i></a>
+              <a href="#"><i class="uil uil-facebook-f"></i></a>
+              <a href="#"><i class="uil uil-dribbble"></i></a>
+          </nav>
+            
+            ';
+            
+            }
+
+            if($id=='7'){
+    
+
+                echo '  
+                
+                <h1 class="modal-title" style="text-align: center;">Suganya Ramesh </h1>
+    
+    
+    
+        
+                <img class="  left-side" src="assets/img/team/suganya.jpg" srcset="assets/img/team/suganya.jpg 2x" alt="" style="object-fit: cover;" /><br>
+  
+                <p>Mrs. Suganya Ramesh has completed her Post Graduation in M.Sc (Software Science) and has over 16 years of experience with a background in both Front-end and Back-end development.
+                </p>
+              
+              
+                <nav class="nav social mb-0">
+                <a href="#"><i class="uil uil-twitter"></i></a>
+                <a href="#"><i class="uil uil-linkedin"></i></a>
+                <a href="#"><i class="uil uil-facebook-f"></i></a>
+                <a href="#"><i class="uil uil-dribbble"></i></a>
+            </nav>
+                
+                ';
+                
+                }
+    
+
+echo  '  ';
+
+?>
